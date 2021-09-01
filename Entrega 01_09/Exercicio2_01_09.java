804733efaae92600d900b711c8b1efa256ac9f30
Bruno Muraro <bmuraro3@gmail.com>
2021-09-01 16:31:45 -0300

    Exercicio atualizado

--- Entrega 01_09/Exercicio2_01_09.java
+++ Entrega 01_09/Exercicio2_01_09.java
@@ -17,23 +17,23 @@ float a, b, c;
 		System.out.println("\nInsira o terceiro número: ");
 		c = leia.nextFloat();
 		
-		if(a>b && b>c)
+		if(a>=b && b>=c)
 		{
 			System.out.println("\nAqui estão os valores em ordem crescente: " + c + " , "+b+" , "+a);
 		}
-		else if(a>c && c>b)
+		else if(a>=c && c>=b)
 		{
 			System.out.println("\nAqui estão os valores em ordem crescente: " + b + " , "+c+" , "+a);
 		}
-		else if(b>a && a>c)
+		else if(b>=a && a>=c)
 		{
 			System.out.println("\nAqui estão os valores em ordem crescente: " + c + " , "+a+" , "+b);
 		}
-		else if(b>c && c>a)
+		else if(b>=c && c>=a)
 		{
 			System.out.println("\nAqui estão os valores em ordem crescente: " + a + " , "+c+" , "+b);
 		}
-		else if(c>b && b>a)
+		else if(c>=b && b>=a)
 		{
 			System.out.println("\nAqui estão os valores em ordem crescente: " + a + " , "+b+" , "+c);
 		}
